200606c0390c1e82bbd573873086b4c0c4121b77
ZackNotZach <hudgens.zack.github@gmail.com>
2015-11-14 15:41:51 -0600

    finished the first draft of the sql file; rune stats table now contains every possible stat you can get from a rune page besides gold/sec, time spent dead, and scaling runes

--- databaseinitial.sql
+++ databaseinitial.sql
@@ -36,8 +36,8 @@ create table Champs_Stats(
 );
 
 create table Item_Stats(
-        item_id varchar(10),
-        health integer,
+    	item_id varchar(10),
+    	health integer,
 	health_regen numeric(3,2),
 	mana integer,
 	mana_regen numeric(3,2),
@@ -58,21 +58,66 @@ create table Item_Stats(
 	spellvamp integer
 );
 
+/* need to determine a way to deal with scaling runes */
+
 create table Rune_Page_Stats(
+    	rune_id varchar(20),
+    	quantity integer,
+    	ability_power integer,
+    	armor integer,
+    	armor_pen integer,
+    	magic_resist integer,
+    	magic_pen integer,
+    	attack_damage integer,
+    	attack_speed integer,
+    	cooldown_reduction integer,
+    	crit_chance integer,
+    	crit_damage integer,
+    	energy integer,
+    	energy_regen numeric(3,2),
+    	health integer,
+    	health_regen numeric(3,2),
+    	percent_health integer,
+    	mana integer,
+    	mana_regen numeric(3,2),
+    	lifesteal integer,
+    	move_speed integer
 );
 
 create table Dragon_Buff_Stats(
-        dragon_num integer,
-        bonus_attack_damage numeric(1,2),
-        bonus_ability_power numeric(1,2),
-        bonus_movement_speed numeric(1,2)
+    	dragon_num integer,
+    	bonus_attack_damage numeric(1,2),
+    	bonus_ability_power numeric(1,2),
+    	bonus_movement_speed numeric(1,2)
 );
 
 create table Current_Stats(
+    	health integer,
+	health_regen numeric(3,2),
+	mana integer,
+	mana_regen numeric(3,2),
+	armor integer,
+	magic_resist integer,
+	attack_damage integer,
+	ability_power integer,
+	flat_armor_pen integer,
+	percent_armor_pen integer,
+	flat_magic_pen integer,
+	percent_magic_pen integer,
+	attack_speed numeric(1,2),
+	flat_move_speed integer,
+	percent_move_speed integer,
+	crit_chance integer,
+	crit_damage integer,
+	energy integer,
+	energy_regen integer,
+	cooldown_reduction integer,
+	lifesteal integer,
+	spellvamp integer
 );
 
 create table Inventory(
-        item_name varchar(30),
-        item_id varchar(10) references Item_Stats(item_id),
-        inventory_num integer
+    	item_name varchar(30),
+    	item_id varchar(10) references Item_Stats(item_id),
+    	inventory_num integer
 );
